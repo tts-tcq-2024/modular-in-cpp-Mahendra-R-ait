1e9b1d11722ca2cd579fcdb1efaf66d45eca3485
Mahendra R <83295768+Mahendra-R-ait@users.noreply.github.com>
2024-09-23 18:36:20 +0530

    Create ColorPair.hpp

---
+++ ColorPair.hpp
@@ -0,0 +1,39 @@
+#ifndef COLORPAIR_H
+#define COLORPAIR_H
+
+#include <iostream>
+#include <sstring>
+
+namespace TelCoColorCoder 
+{
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+    int numberOfMajorColors =
+        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+    int numberOfMinorColors =
+        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+    class ColorPair {
+        private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor major, MinorColor minor):
+                majorColor(major), minorColor(minor)
+            {}
+            MajorColor getMajor() {
+                return majorColor;
+            }
+            MinorColor getMinor() {
+                return minorColor;
+            }
+            std::string ToString();
+    };
+#endif
